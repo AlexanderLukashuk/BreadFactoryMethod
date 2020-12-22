e8da0ae7f2316f4d624729e08608b429f0413a94
Саня <sanya@Sergejs-MacBook-Air.local>
2020-12-22 18:10:58 +0600

    Первое создание

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BreadFactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
